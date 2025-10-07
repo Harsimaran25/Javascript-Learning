895fb51f8b10eef3a332f752b07d983f7ffbbbf5
Hars <harsimaran.singh25@outlook.com>
2025-10-07 14:09:45 +1100

    addedfile

--- Practiceexercises/BasicLesson.js
+++ Practiceexercises/BasicLesson.js
@@ -6,12 +6,27 @@ doing. Anything that requires more than a static web page needs JavaScript*/
 time the code runs */
 //declare variable - you need a special word for that: let, var, or const
 
+/**var has global scope and let has block scope. var's global scope means that you can
+use the variables defined with var in the entire script. On the other hand, let's block
+scope means you can only use variables defined with let in the specific block of code
+in which they were defined 
+
+JavaScript is a loosely typed language. This means that JavaScript determines the type based on the value 
+The type does not need to be named explicitly. For example, if you declared a value of 5,
+JavaScript will automatically define it as a number type.
+
+JavaScript has seven primitives: String, Number, BigInt, Boolean, Symbol, undefined,
+and null.*/
+
 var firstName = "Bhaijaan";
 let lastName = "ClydeKing";
-
+let language = "JavaScript";
+let message = 'let"s learn';
+let message2 = `let"s learn ${language}`;
 console.log("fullname is ", firstName + " " + lastName); // to print at terminal
 console.log(`fullname is ${firstName} ${lastName}`);
-
+console.log(message);
+console.log(message2);
 //we can also declare variables like below
 
 var age, dateOfBirth;
@@ -31,7 +46,8 @@ console.log(age, address);
 const occupation = "AEM PROLEVEL";
 console.log("occupation is" + occupation);
 
-//data types
+/*data types JavaScript has seven primitives: String, Number, BigInt, Boolean, Symbol, undefined,
+and null.*/
 
 let middleName = "Mpower"; //string
 let area = "Bhakha"; //string
@@ -44,3 +60,9 @@ console.log(typeof area);
 console.log(typeof isMaghin);
 console.log(typeof isSingle);
 console.log(typeof numberOfBeetles);
+
+//escape characters
+let str1 = "Hello , what's bhai's game? is it \"maghin\"?";
+console.log(str1);
+let str2 = 'is bhai\'s game ?"magin"';
+console.log(str2);
