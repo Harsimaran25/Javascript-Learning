0cf35f1a1a956a5b74a20aad5a94846b53e65c04
Hars <harsimaran.singh25@outlook.com>
2025-10-10 12:58:09 +1100

    updated

--- Practiceexercises/ClassBasics.js
+++ Practiceexercises/ClassBasics.js
@@ -1,7 +1,13 @@
 // to understand the basic concept of Classes in JS
 
 export class ToPrint {
-  //method in a class
+  //method in a class  now doing the below comment will display the description of the method
+  // when we use it other places and hover over in another file
+
+  /**
+   * This method will print the last name
+   * @param {string} name
+   */
   printName(name) {
     console.log("name is", name);
   }
