5b423dab0a32c5cfb2a02fdeff98b866439af626
Hars <harsimaran.singh25@outlook.com>
2025-10-10 12:52:56 +1100

    updated

---
+++ Practiceexercises/ClassBasics.js
@@ -0,0 +1,8 @@
+// to understand the basic concept of Classes in JS
+
+export class ToPrint {
+  //method in a class
+  printName(name) {
+    console.log("name is", name);
+  }
+}
---
+++ Practiceexercises/ClassBasics2.js
@@ -0,0 +1,13 @@
+// here lets import the class
+
+import { ToPrint } from "./ClassBasics.js";
+
+//now in order to use methods of class we need to create instance/object of class
+
+let printOne = new ToPrint();
+
+printOne.printName("Bhaijaan");
+
+let toprint = new ToPrint();
+
+toprint.printName("KingClyde");
