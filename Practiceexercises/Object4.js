96578e1418c449b51e7a56cffb2a92d77e2ffee7
Hars <harsimaran.singh25@outlook.com>
2025-10-15 12:54:13 +1100

    updated

---
+++ Practiceexercises/Object4.js
@@ -0,0 +1,17 @@
+// object exercise 4 for objects
+
+const ClydeKing = {
+  name: "Clyde King",
+  age: 25,
+  address: [
+    { suburb: "Clyde", number: 420, street: "Clyde King" },
+    {
+      suburb: "Dasua",
+      number: 69,
+      street: "Mpower",
+    },
+  ],
+  greetBhai: function () {
+    console.log("Hello", this.name, address[1].suburb);
+  },
+};
