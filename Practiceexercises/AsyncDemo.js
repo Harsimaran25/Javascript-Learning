b7b1fe99c0a41a0dc7f0ca4a29e80e7490706353
Hars <harsimaran.singh25@outlook.com>
2025-10-16 10:50:59 +1100

    added

---
+++ Practiceexercises/AsyncDemo.js
@@ -0,0 +1,21 @@
+// we can use the following program to prove or demonstrate that Javascript is asynchronous
+
+console.log("this is 1st line");
+console.log("this is 2st line");
+console.log("this is 3st line");
+console.log("this is 4st line");
+
+//now running above it will run and print in sequence
+//let us settimeout function to prove async nature
+//settimeout function takes two arguments, first is the callback function and second is the time in milliseconds after
+//  which the callback function will be executed
+// here we are using arrow function to define the callback function
+// we are using 0 milliseconds to prove that even if we provide 0 milliseconds it will be executed after the current
+// execution context is completed
+
+setTimeout(() => {
+  console.log("this is 5th line");
+}, 2000); //have provided 2000ms which is 2 seconds
+
+console.log("this is 6st line");
+console.log("this is 7st line");
