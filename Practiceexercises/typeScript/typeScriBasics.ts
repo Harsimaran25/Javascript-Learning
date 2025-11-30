7466f6581d0ba05270807bec75975a576113b098
Hars <harsimaran.singh25@outlook.com>
2025-11-30 19:32:27 +1100

    ts

---
+++ Practiceexercises/typeScript/typeScriBasics.ts
@@ -0,0 +1,9 @@
+// this is to learn Typescript basics 
+
+//first program
+
+//Typescript variables  - variable is a container to store data
+
+let age:number =20  // equivalent JS code will be let age=20 
+
+console.log(age)
