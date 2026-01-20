e1e1f18d9eedb1f2eb50aedde127e50ca70fa25a
Hars <harsimaran.singh25@outlook.com>
2026-01-20 19:49:37 +1100

    added

--- Practiceexercises/javaScript/Basics2Javascript.js
+++ Practiceexercises/javaScript/Basics2Javascript.js
@@ -1,3 +1,26 @@
 // This is to  further revise the Java script Basics
 
 console.log('This is first line')
+
+console.log("/////////////////////////////")
+//A JavaScript variable is a container for storing data. h is variable and value is 55
+// let is keyword to declare variable
+let h=55
+
+console.log('Value of h is' ,h)
+
+console.log("/////////////////////////////")
+
+console.log('Let us see some Var keyword example')
+
+let name4='Bhai'
+var name2='Uchhjaatipandit'
+var name3=`${name4} ${name2}`
+console.log(`name is storing ${name4}`)
+
+console.log('name3 stores : ',name3)
+
+let BigNum; // declare variable num
+BigNum=123000 // assign 123000 to BigNum
+
+let num4=7,num33=898 //multiple variable declare in single statment
