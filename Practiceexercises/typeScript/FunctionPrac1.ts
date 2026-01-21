e4325af90947c97820ab0a884c2ad6fd5f6191db
Hars <harsimaran.singh25@outlook.com>
2026-01-21 22:16:27 +1100

    addedd

--- Practiceexercises/typeScript/FunctionPrac1.ts
+++ Practiceexercises/typeScript/FunctionPrac1.ts
@@ -43,11 +43,56 @@ console.log("Outside function");
 /*Arguments
 Arguments are values you pass to the function when you call it.*/
 
-//function with a parameter
+// Function with a string parameter called 'name', 
+// name is a function parameter, which acts as a placeholder to store the function argument.
 
 function greetName(name : string) : void {
 
     console.log(`hello ${name}`)
 }
 
-greetName('ClydeKing')//call the function
+greetName('ClydeKing')//call the function // Pass argument to the function the argument "ClydeKing" is stored in the name parameter.
+
+//function to add two numbers 
+
+function addNums(a: number,b:number) : void{
+    let sum = a+b
+    console.log(`sum is ${sum}`)
+}
+
+addNums(4,3)
+
+//anonymous funtcion 
+
+let myAdd= function (x:number, y:number) : number {
+    return x+y
+}
+console.log(myAdd(4,4))
+
+//lets see return statement We can return a value from a TypeScript function using the return statement
+
+//function to return square of a number
+//TypeScript can figure the return type out by looking at the return statements, so we can also optionally leave this off in many cases.
+function sQuare (j:number) : number {
+ return j*j;
+
+}
+
+console.log(sQuare(6))  //OR 
+
+let sqofNum= sQuare(6)
+console.log(`square is ${sqofNum}`)
+
+/**The return Statement Terminates the Function
+Any code written in the function after the return statement is not executed. */
+function display(): string {
+
+    console.log("This will be executed.");
+
+    return "Returning from function.";
+
+    console.log("This will not be executed.");
+}
+
+let message: string = display();
+console.log(message);
