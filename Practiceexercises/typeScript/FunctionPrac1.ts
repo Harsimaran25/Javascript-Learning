e07d44d62169968cdfa4860246ea0b4f4c1226c6
Hars <harsimaran.singh25@outlook.com>
2026-01-21 21:27:28 +1100

    addedd

--- Practiceexercises/typeScript/FunctionPrac1.ts
+++ Practiceexercises/typeScript/FunctionPrac1.ts
@@ -27,3 +27,27 @@ let subtract=(a:number,b:number):number=>{return a-b};
 let s=subtract(10,5);
 console.log(s);
 
+// function to print greeting message
+//The function keyword is used to create the function.
+//We can create a function in TypeScript using the function keyword and specifying a return type:
+
+function greetFunc() : void {  // greetFunc is name of function and void is return type , void which means it doesn't return any value.
+
+console.log('hi from Greet function')// body of function
+}
+
+// Call the function
+greetFunc()  // calling the function
+console.log("Outside function");
+
+/*Arguments
+Arguments are values you pass to the function when you call it.*/
+
+//function with a parameter
+
+function greetName(name : string) : void {
+
+    console.log(`hello ${name}`)
+}
+
+greetName('ClydeKing')//call the function
